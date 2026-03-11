f0756c55804a80f9a9e813ea67471c815da9687b
sohag142 <sohag2879@gmail.com>
2026-03-11 16:42:22 +0600

    envioment setup

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDAru7AcCgrzbO84HZl3HOrBNp1dyfcmZM",
-  authDomain: "global-trade-hab.firebaseapp.com",
-  projectId: "global-trade-hab",
-  storageBucket: "global-trade-hab.firebasestorage.app",
-  messagingSenderId: "1060533692264",
-  appId: "1:1060533692264:web:be4acd89a3399fbd4998dd"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
